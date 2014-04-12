8a7378318fbb870db494cd62b7f371eceac446ee
Darin Critchlow <darincritchlow@gmail.com>
2014-04-11 22:57:02 -0600

    Put currency.py in correct folder

--- tests/currency_tests.py
+++ tests/currency_tests.py
@@ -1,6 +1,10 @@
 from nose.tools import *
-from GERMANY.germany import Currency
+from GERMANY.currency import Currency
 
 def test_dollars():
     dollars = Currency('dollars')
     assert_equal(dollars.currency_type, 'dollars')
+
+def test_euros():
+    euros = Currency('euros')
+    assert_equal(euros.currency_type, 'euros')
